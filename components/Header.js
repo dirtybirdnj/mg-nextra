cb0fbbb62bf76020d13df75ffbdcb2a996d9f559
Nicole Colabella <>
2023-01-31 16:52:21 -0500

    nav work in progress

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { Container, Row, Col, Button, Text, Image, Navbar } from "@nextui-org/react";
+import { Buy } from 'react-iconly'
+import { Spacer, Container, Row, Col, Button, Text, Image, Navbar } from "@nextui-org/react";
 
 const Header = ({
   pageMap
@@ -31,22 +32,26 @@ const Header = ({
   }, [pageMap])
 
   return (
-    <Container css={{paddingTop: '30px', paddingBottom: '30px'}}>
-      <Row justify="space-between" align="center">
-        <Col span={2}>
-          <Image src="/img/vtj-circle.svg" alt="verticaltubejig.com" />
-          <Text h4>verticaltubejig.com</Text>
-        </Col>
-        <Col css={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>  
-          <Button className="snipcart-checkout">View Cart 🛒</Button>        
-          <Navbar isCompact disableShadow disableBlur containerCss={{display: 'flex', justifyContent: 'flex-end'}}>
-            <Navbar.Content>
-              {navItems}
-            </Navbar.Content>
-          </Navbar>          
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Spacer y={2} />
+      <Container>
+        <Row justify="space-between" align="center">
+          <Col span={2}>
+            <Image src="/img/vtj-circle.svg" alt="verticaltubejig.com" />
+            <Text h4>verticaltubejig.com</Text>
+          </Col>
+          <Col css={{display: 'flex', alignItems: 'center'}}>          
+            <Navbar isCompact disableShadow disableBlur containerCss={{display: 'flex', justifyContent: 'flex-end'}}>
+              <Navbar.Content>
+                {navItems}
+              </Navbar.Content>
+            </Navbar>  
+            <Button size="sm" bordered auto icon={<Buy primaryColor="currentColor" set="broken" />}>View Cart</Button>
+          </Col>
+        </Row>
+      </Container>
+      <Spacer y={2} />
+    </>
   )
 };
 
